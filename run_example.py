0025461f7773546ed2627ebfd0c1acfb06ca5d93
Harald G Svendsen <harald.svendsen@sintef.no>
2020-01-16 15:44:59 +0100

    added run script for single step (testing)

--- run_example.py
+++ run_example.py
@@ -41,67 +41,40 @@ mc = multicarrier.Multicarrier(loglevel="INFO")
 datafile = "data_example.xlsx"
 data,profiles = multicarrier.read_data_from_xlsx(datafile,carrier_properties)
 
-runSingle = False
-if runSingle:
-    print("Without battery")
-    #mc = multicarrier.Multicarrier(loglevel="INFO")
-    data['paramDevice'][17]['Emax']=0 # MWh - battery
-    instance = mc.createModelInstance(data,profiles,filename="model0.txt")
-    sol = mc.solve(solver="cbc",write_yaml=False)
-    multicarrier.Plots.plotDeviceSumPowerLastOptimisation(instance,filename="a_devsum_el0.png")
-    multicarrier.Plots.plotDevicePowerLastOptimisation1(mc,device=17,filename="a_battery0.png")
-    sumCO2 = multicarrier.pyo.value(mc.compute_CO2(instance))
-    print("CO2 emitted = {} kg/hour".format(sumCO2))
-    
-    print("With battery")
-    #mc = multicarrier.Multicarrier(loglevel="INFO")
-    #data,profiles = multicarrier.read_data_from_xlsx(datafile,carrier_properties)
-    data['paramDevice'][17]['Emax']=2.5 # MWh - battery
-    instance = mc.createModelInstance(data,profiles)
-    mc.instance.paramDeviceEnergyInitially[17]=2.5 #MWh (fully charged at start)
-    sol = mc.solve(solver="cbc",write_yaml=False)
-    multicarrier.Plots.plotDeviceSumPowerLastOptimisation(instance,filename="b_devsum_el0.png")
-    multicarrier.Plots.plotDevicePowerLastOptimisation1(mc,device=17,filename="b_battery0.png")
-    sumCO2 = multicarrier.pyo.value(mc.compute_CO2(instance))
-    print("CO2 emitted = {} kg/hour".format(sumCO2))
-    
-    multicarrier.Plots.plotNetworkCombined(mc)
-    print("Done")
+#data['paramDeviceEnergyInitially'][17]=2.5 # MWh - battery
+instance = mc.createModelInstance(data,profiles)
 
-runMany=True
-if runMany:
-    #data['paramDeviceEnergyInitially'][17]=2.5 # MWh - battery
-    instance = mc.createModelInstance(data,profiles)
-    
-    status = mc.solveMany(solver="cbc",time_end=48,write_yaml=False)
-    mc.instance.pprint(filename="model.txt")
-    
-    multicarrier.Plots.plotNetworkCombined(mc)
-    multicarrier.Plots.plotNetworkCombined(mc,only_carrier='el')
-    multicarrier.Plots.plotNetworkCombined(mc,only_carrier='gas')
-    multicarrier.Plots.plotNetworkCombined(mc,only_carrier='heat')
-    
-    multicarrier.Plots.plotDeviceSumPowerLastOptimisation(instance,
-                                                          filename="devsum_el.png")
-    #multicarrier.Plots.plotDeviceSumPowerLastOptimisation(instance,carrier="gas",
-    #                                                      filename="devsum_gas.png")
-    multicarrier.Plots.plotEmissionRateLastOptimisation(instance,filename="co2out.png")
-    #multicarrier.Plots.plotNetworkCombined(instance,timestep=18,filename="t18.png")
-    
-    # Show device output vs available power for wind turbine:
-    #multicarrier.Plots.plotDevicePowerLastOptimisation1(mc,device=7,
-    #                                                      filename="wind.png")
-    
-    multicarrier.Plots.plotProfiles(profiles,filename="profiles.png")
-    
-    sumCO2 = multicarrier.pyo.value(mc.compute_CO2(instance))
-    print("last optimisation: CO2 emitted = {} kg".format(sumCO2))
-    
-    #plots.plot_df(mc._dfDevicePower,id_var="device",filename="plotly.html",
-    #              title="Device Power",ylabel="Power (MW)")
-    
-    plots.plot_SumPowerMix(mc,carrier="el",filename="el_sum_opt.png")
-    plots.plot_deviceprofile(mc,dev=7,profiles=profiles) # wind
-    #plots.plot_deviceprofile(mc,dev=17,profiles=profiles) # battery
-    plots.plot_CO2(mc,filename="co2rate_opt.png")
-    plots.plot_devicePowerEnergy(mc,17,filename="battery_opt.png")
+status = mc.solveMany(solver="cbc",time_end=48,write_yaml=False)
+
+
+# Analyse results:
+
+multicarrier.Plots.plotNetworkCombined(mc)
+multicarrier.Plots.plotNetworkCombined(mc,only_carrier='el')
+multicarrier.Plots.plotNetworkCombined(mc,only_carrier='gas')
+multicarrier.Plots.plotNetworkCombined(mc,only_carrier='heat')
+
+multicarrier.Plots.plotDeviceSumPowerLastOptimisation(instance,
+                                                      filename="devsum_el.png")
+#multicarrier.Plots.plotDeviceSumPowerLastOptimisation(instance,carrier="gas",
+#                                                      filename="devsum_gas.png")
+multicarrier.Plots.plotEmissionRateLastOptimisation(instance,filename="co2out.png")
+#multicarrier.Plots.plotNetworkCombined(instance,timestep=18,filename="t18.png")
+
+# Show device output vs available power for wind turbine:
+#multicarrier.Plots.plotDevicePowerLastOptimisation1(mc,device=7,
+#                                                      filename="wind.png")
+
+multicarrier.Plots.plotProfiles(profiles,filename="profiles.png")
+
+sumCO2 = multicarrier.pyo.value(mc.compute_CO2(instance))
+print("last optimisation: CO2 emitted = {} kg".format(sumCO2))
+
+#plots.plot_df(mc._dfDevicePower,id_var="device",filename="plotly.html",
+#              title="Device Power",ylabel="Power (MW)")
+
+plots.plot_SumPowerMix(mc,carrier="el",filename="el_sum_opt.png")
+plots.plot_deviceprofile(mc,dev=7,profiles=profiles) # wind
+#plots.plot_deviceprofile(mc,dev=17,profiles=profiles) # battery
+plots.plot_CO2(mc,filename="co2rate_opt.png")
+plots.plot_devicePowerEnergy(mc,17,filename="battery_opt.png")
