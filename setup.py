480e7b88cb9761975d663890fc40dc87c52debae
Wouter J. de Bruin <wouterjdb@hotmail.com>
2021-05-19 15:44:17 +0200

    Structural changes

--- setup.py
+++ setup.py
@@ -1,18 +1,50 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
-exec(open('oogeso/version.py').read())
+with open("README.md", "r") as fh:
+    LONG_DESCRIPTION = fh.read()
+
+REQUIREMENTS = [
+    "dash",
+	"flask",
+	"matplotlib",
+	"networkx",
+    "numpy",
+    "pandas",
+	"plotly",
+	"pydot",
+	"pyyaml",
+	"Pyomo",
+    "scipy",
+	"seaborn",
+	"tqdm",
+	"xlrd",
+]
+
+TEST_REQUIRES = [
+    "black",
+    "mypy",
+    "pylint",
+    "pytest",
+]
 
 setup(name='oogeso',
-      version=__version__,
+      install_requires=REQUIREMENTS,
+	  tests_require=TEST_REQUIRES,
+	  setup_requires=["setuptools_scm~=3.2"],      
       description='Offshore Oil and Gas Field Energy System Operational Optimisation (OOGESO)',
+	  long_description=LONG_DESCRIPTION,
+	  url="https://github.com/oogeso/oogeso",
+	  use_scm_version=True,
       license='MIT License (http://opensource.org/licenses/MIT)',
-      packages=['oogeso'],
-      zip_safe = True,
+      package_dir={"": "src"},
+	  packages=find_packages("src"),
+      zip_safe = False,
 	  classifiers = [
-		'Development Status :: 3 - Alpha',
-		'Intended Audience :: Science/Research',
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+        "Natural Language :: English",
+        "Topic :: Scientific/Engineering",
 		'License :: OSI Approved :: MIT License',
-		'Programming Language :: Python :: 3'
 	  ],
 	  keywords = 'offshore energy system, oil and gas, operational optimisation',
 	 )
