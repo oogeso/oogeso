172443f6b513117fa83f5c82837a2f5aae6eb7e2
Harald G Svendsen <harald.svendsen@sintef.no>
2021-05-26 11:05:15 +0200

    code style (black)

--- src/oogeso/io/file_io.py
+++ src/oogeso/io/file_io.py
@@ -4,8 +4,9 @@ import yaml
 import pandas as pd
 import numpy as np
 import logging
-#from oogeso.core import electricalsystem
-#from core import milp_compute
+
+# from oogeso.core import electricalsystem
+# from core import milp_compute
 
 
 def _convert_xls_input(df, columns, index_col="id"):
