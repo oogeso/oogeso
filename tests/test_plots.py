d09785b329e6e0e6712137f47f7681c4dd9f8099
Harald G Svendsen <harald.svendsen@sintef.no>
2021-10-26 11:34:32 +0200

    linting fix

--- tests/test_plots.py
+++ tests/test_plots.py
@@ -1,20 +1,20 @@
 def test_plots():
     # Test all plotting methods, using a test dataset
-    
-    #res = ...
-    #plots.plot_deviceprofile(res,["GT_A1","GT_A2","GT_A3","el_demand_A"])
-    #plots.plot_devicePowerEnergy(res,"GT_A1")
-    #plots.plot_SumPowerMix(res,carrier="el")
-    #plots.plot_ExportRevenue(res) # gives error because it is zero in this case
-    #plots.plot_CO2rate(res)
-    #plots.plot_CO2rate_per_dev(res)
-    #plots.plot_CO2_intensity(res)
-    #plots.plotProfiles(data.profiles)
-    #plots.plotDevicePowerFlowPressure(res,"GT_A3")
-    #plots.plotNetwork(res)
-    #plots.plotGasTurbineEfficiency()
-    #plots.plotReserve(res)
-    #plots.plotElBackup(res)
-    #plots.plotElBackup2(res).set_ylim([0,25]) #differs somewhat from the above
-    
+
+    # res = ...
+    # plots.plot_deviceprofile(res,["GT_A1","GT_A2","GT_A3","el_demand_A"])
+    # plots.plot_devicePowerEnergy(res,"GT_A1")
+    # plots.plot_SumPowerMix(res,carrier="el")
+    # plots.plot_ExportRevenue(res) # gives error because it is zero in this case
+    # plots.plot_CO2rate(res)
+    # plots.plot_CO2rate_per_dev(res)
+    # plots.plot_CO2_intensity(res)
+    # plots.plotProfiles(data.profiles)
+    # plots.plotDevicePowerFlowPressure(res,"GT_A3")
+    # plots.plotNetwork(res)
+    # plots.plotGasTurbineEfficiency()
+    # plots.plotReserve(res)
+    # plots.plotElBackup(res)
+    # plots.plotElBackup2(res).set_ylim([0,25]) #differs somewhat from the above
+
     assert True
