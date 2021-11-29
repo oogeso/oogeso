194492e5d327020906332ffc2d1f6a7d27460289
Morten Dæhli Aslesen <mortendaehli@gmail.com>
2021-11-29 11:07:37 +0100

    CI: Build and test CBC-optimizer in separate pipeline (#47)

--- Dockerfile
+++ Dockerfile
@@ -1,11 +1,20 @@
 FROM python:3.9 as dev
 
+ARG INSTALL_DEV=true
+ENV PYTHONUNBUFFERED=1
+
 RUN apt-get update && \
   apt-get -y upgrade && \
   rm -rf /var/lib/apt/lists/* \
 
-ARG INSTALL_DEV=false
-ENV PYTHONUNBUFFERED=1
+WORKDIR /coinbrew
+
+# Download repository with CBC solver and intstall
+RUN git clone https://github.com/coin-or/coinbrew /var/cbc
+WORKDIR /var/cbc
+RUN ./coinbrew fetch Cbc:stable/2.10 --no-prompt --no-third-party
+RUN ./coinbrew build Cbc --no-prompt --no-third-party --prefix=/usr
+ENV COIN_INSTALL_DIR /usr
 
 WORKDIR /code
 
@@ -13,16 +22,15 @@ COPY pyproject.toml poetry.lock ./
 
 RUN pip install poetry && poetry config virtualenvs.create false
 
-WORKDIR /code
+RUN bash -c "if [ $INSTALL_DEV == 'true' ] ; then poetry install --no-root ; else poetry install --no-root --no-dev ; fi"
 
-COPY pyproject.toml poetry.lock ./
-
-RUN bash -c "if [ INSTALL_DEV == 'true' ] ; then poetry install --no-root ; else poetry install --no-root --no-dev ; fi"
-
-ENV PYTHONPATH=/code
-
-FROM build AS tests
+FROM dev as test
 
 COPY ./ ./
+ENV PYTHONPATH=/code/src
+
+FROM test AS build
 
 RUN poetry install --no-dev
+
+
--- tests/test_testcase.py
+++ tests/test_testcase.py
@@ -1,10 +1,6 @@
 from pathlib import Path
 import oogeso
 import oogeso.io
-import pyomo.opt as pyopt
-import pyomo.environ as pyo
-import pytest
-
 
 TEST_DATA_ROOT_PATH = Path(__file__).parent
 
@@ -16,22 +12,3 @@ def test_simulator_create():
     assert isinstance(simulator, oogeso.Simulator)
     assert isinstance(simulator.optimiser, oogeso.OptimisationModel)
     # assert isinstance(simulator.optimiser.pyomo_instance)
-
-
-def test_simulator_run():
-    data = oogeso.io.read_data_from_yaml(TEST_DATA_ROOT_PATH / "testdata1.yaml")
-    simulator = oogeso.Simulator(data)
-    # Continue test only if cbc executable is present
-    opt = pyo.SolverFactory("cbc")
-    if not opt.available():
-        pytest.skip("CBC executable not found. Skipping test")
-
-    sol = simulator.optimiser.solve(solver="cbc", timelimit=20)
-    assert (
-        sol.solver.termination_condition == pyopt.TerminationCondition.optimal
-    ), "Optimisation with CBC failed"
-    res = simulator.runSimulation("cbc", timerange=[0, 1], timelimit=20)
-    assert (
-        res.device_flow.loc[("dem", "el", "in", 0)]
-        == res.device_flow.loc[("source1", "el", "out", 0)]
-    )
