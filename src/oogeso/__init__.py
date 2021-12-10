7e6d4b32bba37e5c52854d2b36216075e5f0072d
morten.aslesen <mortendaehli@gmail.com>
2021-12-10 17:41:11 +0100

    fix: Avoid importing plotting libraries by default

--- src/oogeso/__init__.py
+++ src/oogeso/__init__.py
@@ -1,4 +1,4 @@
 from oogeso.core import OptimisationModel, Simulator
 
 from . import core, dto, io, utils
-from .core import devices, milp_plot, networks, optimiser, simulator
+from .core import devices, networks, optimiser, simulator
