d4fb56a54147b06cbb5ee4e954639e525404c0ec
Harald G Svendsen <harald.svendsen@sintef.no>
2024-05-27 14:14:20 +0200

    add test

--- tests/test_gasturbine_hydrogen.py
+++ tests/test_gasturbine_hydrogen.py
@@ -3,7 +3,6 @@ import pytest
 
 import oogeso
 import oogeso.dto as dto
-from oogeso.core.devices import GasTurbine
 
 from .conftest import TEST_DATA_ROOT_PATH
 
@@ -16,17 +15,22 @@ def testcase_gasturbine_hydrogen_data() -> oogeso.dto.base.EnergySystemData:
     return oogeso.io.read_data_from_yaml(TEST_DATA_ROOT_PATH / "testdata_gasturbine_hydrogen.yaml")
 
 
-def test_gasturbine_hydrogen_model():
-    dev_obj = GasTurbine()
-    # TODO add tests
-    assert 1==0
-
+def test_gasturbine_hydrogen_model(testcase_gasturbine_hydrogen_data: dto.EnergySystemData):
+    my_sim = oogeso.Simulator(testcase_gasturbine_hydrogen_data)
+    dev_gtg = my_sim.optimiser.all_devices["gtg"]
+    assert dev_gtg.dev_data.hydrogen_blend_max == 0.2
 
 
 @pytest.mark.skipif(not pyo.SolverFactory("cbc").available(), reason="Skipping test because CBC is not available.")
 def test_gasturbine_hydrogen_simple_simulation(testcase_gasturbine_hydrogen_data: dto.EnergySystemData):
     my_sim = oogeso.Simulator(testcase_gasturbine_hydrogen_data)
     res = my_sim.run_simulation("cbc", time_range=(0, 4))
-    # TODO add tests
-    assert 1==0
 
+    hydrogen_fraction = res.device_flow["gtg", "hydrogen", "in", 0] / (
+        res.device_flow["gtg", "hydrogen", "in", 0] + res.device_flow["gtg", "gas", "in", 0]
+    )
+    assert hydrogen_fraction == 0.2  # max value
+
+    assert res.device_flow["gtg", "gas", "in", 0] == pytest.approx(0.5433526, rel=1e-6)
+    assert res.device_flow["gtg", "hydrogen", "in", 0] == pytest.approx(0.13583815, rel=1e-6)
+    assert res.device_flow["gtg", "carbon", "out", 0] == pytest.approx(1.2714451, rel=1e-6)
